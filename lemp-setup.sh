2fd4c6026c117c5eac3f0a8ab04ef8c7e92d845e
dannylanda <79216206+dannylanda@users.noreply.github.com>
2024-06-12 15:31:32 +0100

    lemp-setup.sh
    updated path of testing.txt to /root/testing.txt

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt -y upgrade
-sudo touch testing.txt # this file will contain the output of our LEMP stack unit tests
+sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
 sudo apt -y install nginx
 sudo systemctl start nginx && systemctl enable nginx
 sudo systemctl status nginx > testing.txt
 sudo apt -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
-sudo systemctl status mariadb >> testing.txt
+sudo systemctl status mariadb >> /root/testing.txt
 sudo apt install -y php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
-php -v >> testing.txt
+php -v >> /root/testing.txt
