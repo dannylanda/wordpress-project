5acc3296965cbd56ca3c8ca621b7011f50f3c6e8
dannylanda <79216206+dannylanda@users.noreply.github.com>
2024-06-12 15:37:55 +0100

    Update lemp-setup.sh
    final script

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -3,10 +3,10 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
 sudo apt -y install nginx
-sudo systemctl start nginx && systemctl enable nginx
-sudo systemctl status nginx > testing.txt
+sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
+sudo systemctl status nginx > /root/testing.txt
 sudo apt -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
-sudo systemctl status mariadb >> /root/testing.txt
-sudo apt install -y php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
-php -v >> /root/testing.txt
+systemctl status mariadb >> /root/testing.txt
+sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
+sudo php -v >> /root/testing.txt
