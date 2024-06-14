e3880e6bf099f89124d551b136460958d7ebf29b
dannylanda <79216206+dannylanda@users.noreply.github.com>
2024-06-14 11:26:09 +0100

    Update lemp-setup.sh
    added comments

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,13 +1,40 @@
-#!/bin/bash
+#!/bin/bash 
+
+# Update the package list
 sudo apt -y update
+
+# Upgrade all installed packages to their latest versions
 sudo apt -y upgrade
-sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
+
+# Create a file to store the output of LEMP stack unit tests
+sudo touch /root/testing.txt
+
+# Install Nginx web server
 sudo apt -y install nginx
-sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
+
+# Start Nginx and enable it to start on server boot
+sudo systemctl start nginx && sudo systemctl enable nginx
+
+# Write the status of Nginx to the testing.txt file
 sudo systemctl status nginx > /root/testing.txt
+
+# Install MariaDB server
 sudo apt -y install mariadb-server
+
+# Start MariaDB and enable it to start on server boot
 sudo systemctl start mariadb && sudo systemctl enable mariadb
+
+# Append the status of MariaDB to the testing.txt file
 systemctl status mariadb >> /root/testing.txt
+
+# Install PHP and various PHP extensions
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
+
+# Append the PHP version information to the testing.txt file
 sudo php -v >> /root/testing.txt
-sudo mv index.html index.html.old #apache is installed by default with this install, renamed the apache test page
+
+# Change directory to /var/www/html/
+cd /var/www/html/
+
+# Rename the default Apache test page if it exists
+sudo mv index.html index.html.old
