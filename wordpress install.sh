68c437ae8f9ea5f2b409e41627e678d4ceef7370
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2024-06-19 15:20:48 +0100

    Update wordpress install.sh

--- wordpress install.sh
+++ wordpress install.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-sudo cd /var/www/html
-sudo apt -y install unzip
+sudo cd /var/www/html/
 sudo wget https://wordpress.org/latest.zip
+sudo apt install unzip
 sudo unzip latest.zip
-rm latest.zip
-sudo mysql -u root
-# create database wordpress;
-# create user wpuser@localhost identified by 'your-password';
-# grant all privileges on wordpress.* to wpuser@localhost;
-# flush privileges;
-# exit;
-# wget wp=config.php from S3 Bucket
+sudo rm latest.zip
+sudo mariadb -u root
+
+# /** The name of the database for WordPress */
+define('DB_NAME', 'database_name_here');
+#/** MySQL database username */
+define('DB_USER', 'username_here');
+/** MySQL database password */
+define('DB_PASSWORD', 'password_here');
+
+cd wordpress/ 
+sudo cp wp-config-sample.php wp-config.php
+vi wp-config.php
+# added the database name / username / password
+
 sudo chmod 640 wp-config.php
-sudo cd /etc/nginx/conf.d/
-# sudo touch wordpress.conf pull from S3 Bucket
+sudo chown www-data:www-data /var/www/html/wordpress/ -R
+
+cd /etc/nginx/conf.d/
+sudo vi /etc/nginx/conf.d/wordpress.conf
+# ec2-54-83-68-228.compute-1.amazonaws.com;
+# Root /var/www/html/wordpress/;
+nginx -t
+systemctl reload nginx
