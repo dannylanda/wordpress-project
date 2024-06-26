2f67be3ee64b94d55703eface4297f194bfe8de9
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2024-06-26 14:45:13 +0100

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -3,9 +3,6 @@
 # Install git package without prompting for confirmation
 sudo apt -y install git
 
-# Change directory to /root/
-sudo cd /root/
-
 # Clone the specified GitHub repository into /root/
 sudo git clone https://github.com/dannylanda/wordpress-project.git /root
 
