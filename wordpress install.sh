58783383d7b3c3a3ff4180ece7b7a9e86a1e43ba
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2024-06-26 13:12:06 +0100

    Update wordpress install.sh
    added final database automation

--- wordpress install.sh
+++ wordpress install.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-sudo cd /var/www/html/
+sudo cd /var/www/
+sudo rm -rf html
 sudo wget https://wordpress.org/latest.zip
 sudo apt install unzip
 sudo unzip latest.zip
 sudo rm latest.zip
-sudo mariadb -u root
-
-# /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
-#/** MySQL database username */
-define('DB_USER', 'username_here');
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
-
-cd wordpress/ 
-sudo cp wp-config-sample.php wp-config.php
-vi wp-config.php
-# added the database name / username / password
-
+sudo mv wordpress html
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER wpuser@localhost identified by 'password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
+sudo cd /var/www/html/wordpress
+sudo wget https://dannylandawordpress.s3.amazonaws.com/wp-config.php
 sudo chmod 640 wp-config.php
 sudo chown www-data:www-data /var/www/html/wordpress/ -R
-
-cd /etc/nginx/conf.d/
-sudo vi /etc/nginx/conf.d/wordpress.conf
-# ec2-54-83-68-228.compute-1.amazonaws.com;
-# Root /var/www/html/wordpress/;
-nginx -t
-systemctl reload nginx
