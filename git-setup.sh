595dbb7c9ac0d2782f6f7720c59b77c3d70a6aa7
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2024-07-03 12:02:59 +0100

    Update git-setup.sh
    defined the route it will clone too

--- git-setup.sh
+++ git-setup.sh
@@ -4,7 +4,7 @@
 sudo apt -y install git
 
 # Clone the specified GitHub repository into /root/
-sudo git clone https://github.com/dannylanda/wordpress-project.git wordpress-project
+sudo git clone https://github.com/dannylanda/wordpress-project.git /root/wordpress-project
 
 # Change permissions of the cloned directory to read and execute for all users, and write for the owner
 sudo chmod -R 755 wordpress-project
