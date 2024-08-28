70e6680798af6b8f60a8ad2bc78aebdcb19e10fc
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2024-08-28 15:23:33 +0100

    Update wordpress-install.sh
    testing the updated wordpress-install 
    
    added password and username variable and stopped pulling from the S3 bucket

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -6,15 +6,28 @@ sudo unzip /var/www/latest.zip -d /var/www/
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html 
 
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
+
 # sudo mariadb -u root
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by 'password'"
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
 
-sudo wget -O /var/www/html/wp-config.php https://dannylandawordpress.s3.amazonaws.com/wp-config.php
+# sudo wget -O /var/www/html/wp-config.php https://dannylandawordpress.s3.amazonaws.com/wp-config.php
+
+sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/
+
+# Replace the placeholder 'password_here' in wp-config.php with the generated password.
+sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
 
 # sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
